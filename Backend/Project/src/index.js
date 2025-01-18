9adc915cca3af733b4fec6dc4ae7311ffdc444bf
Rishabh Munjal <rishabh5566ind1111@gmail.com>
2025-01-18 21:06:17 +0530

    proj swtup

---
+++ Backend/Project/src/index.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config({
+    path : './env'
+})
+
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 , ()=>{
+        console.log('Server is Running')
+    })
+})
+.catch((err) =>{
+    console.log("connectDB error" , err)
+})
+
+
